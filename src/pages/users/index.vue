ce264fae28b3c70e414244feb1a80b9795b5d08f
Fantilong <2554509132@qq.com>
2026-06-14 16:09:30 +0800

    集成unplugin-auto-import 和 vueuse hook 库

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="text-5xl font-bold underline flex-1">users/index.vue</div>
 
-  <div class="p-4">hello, users!</div>
+  <div class="p-4">hello, users! {{ msg }} {{ x }}, {{ y }}</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('hello, users!')
+const { x, y } = useMouse()
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,34 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter({}), vue(), vueJsx(), vueDevTools(), UnoCSS()],
+  plugins: [
+    VueRouter({}),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS({}),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
