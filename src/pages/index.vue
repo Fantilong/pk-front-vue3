fc2fa5a6535ec4aa620aaa71714db7e4ff8e0912
Fantilong <2554509132@qq.com>
2026-08-19 15:16:35 +0800

    feat: 课程列表

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,35 @@
 <template>
   <div>
     <Swiper :items="items" :height="36 * store.rate + 'rem'"></Swiper>
-    <Card :image="bg" image-type="avatar" title="title2" sub-title="subTitle1" border></Card>
+    <Container>
+      <!-- 标题 -->
+    </Container>
+    <Container>
+      <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 w-full lt-sm:px-4">
+        <!-- for遍历的内容： 课程 -->
+        <a
+          :href="item.url"
+          v-for="(item, index) in projects"
+          :key="index"
+          target="_blank"
+          class="flex"
+        >
+          <Card
+            class="w-full rounded-0"
+            :icon="item.icon"
+            image-type="rounded"
+            :title="item.title"
+            :sub-title="item.subTitle"
+            border
+          >
+            <div class="mx-4 flex items-center justify-between text-gray-500 mb-4">
+              查看更多
+              <div class="i-mdi:arrow-right-thin"></div>
+            </div>
+          </Card>
+        </a>
+      </div>
+    </Container>
   </div>
 </template>
 
@@ -12,6 +40,39 @@ import bg from '@/assets/images/bg.png'
 import { useThemeStore } from '@/stores/useThemeStore'
 
 const store = useThemeStore()
+
+const projects = [
+  {
+    title: '前端高级工程师',
+    subTitle: 'sdfasdfasdfasfasdfasdfasdfasd',
+    url: 'https://class.imooc.com/sale/fesenior',
+    icon: 'i-mdi:web',
+  },
+  {
+    title: '前端高级工程师',
+    subTitle: 'sdfasdfasdfasfasdfasdfasdfasd',
+    url: 'https://class.imooc.com/sale/fesenior',
+    icon: 'i-mdi:web',
+  },
+  {
+    title: '前端高级工程师',
+    subTitle: 'sdfasdfasdfasfasdfasdfasdfasd',
+    url: 'https://class.imooc.com/sale/fesenior',
+    icon: 'i-mdi:web',
+  },
+  {
+    title: '前端高级工程师',
+    subTitle: 'sdfasdfasdfasfasdfasdfasdfasd',
+    url: 'https://class.imooc.com/sale/fesenior',
+    icon: 'i-mdi:web',
+  },
+  {
+    title: '前端高级工程师',
+    subTitle: 'sdfasdfasdfasfasdfasdfasdfasd',
+    url: 'https://class.imooc.com/sale/fesenior',
+    icon: 'i-mdi:web',
+  },
+]
 
 const items: SwiperItemType[] = [
   {
